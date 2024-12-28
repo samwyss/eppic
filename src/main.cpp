824c207441314febae880e97c1ffee931420bcf9
Sam Wyss <sjwyss@outlook.com>
2024-12-27 21:52:07 -0500

    added basic cmake structure

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+/*!
+* main driver function and build target
+ * @param argc argument count
+ * @param argv argument vector
+ * @return
+ */
+int main(int argc, char** argv) {
+  cout << "Hello World!\n";
+  return 0;
+}
