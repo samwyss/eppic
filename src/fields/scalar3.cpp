880f3e28e7785826742cbe400fcfaf212808e29c
Sam <sjwyss@outlook.com>
2024-12-28 23:35:43 -0500

    began to define Scalar3

--- src/fields/scalar3.cpp
+++ src/fields/scalar3.cpp
@@ -1,1 +1,48 @@
 #include "scalar3.hpp"
+
+template <typename T>
+Scalar3<T>::Scalar3(const size_t num_x, const size_t num_y, const size_t num_z, const T value) :
+    num_cells(Triplet<size_t>{num_x, num_y, num_z}), row_cells(num_z), plane_cells(num_y * num_z),
+    vol_cells(num_x * num_y * num_z)
+{
+    // create data array
+    data = new T[num_x * num_y * num_z];
+
+    // initialize data array with value with overloaded assignment operator
+    (*this) = value;
+}
+
+template <typename T>
+Scalar3<T>::~Scalar3()
+{
+    delete[] data;
+}
+
+template <typename T>
+T* Scalar3<T>::operator()(const size_t i, const size_t j, const size_t k)
+{
+    return &data[k + row_cells * j + plane_cells * i];
+}
+
+template <typename T>
+const T& Scalar3<T>::operator()(const size_t i, const size_t j, const size_t k) const
+{
+    return data[k + row_cells * j + plane_cells * i];
+}
+
+template <typename T>
+Scalar3<T>& Scalar3<T>::operator=(const T& rhs)
+{
+    // assign data array with rhs
+    for (size_t i = 0; i < vol_cells; ++i)
+    {
+        data[i] = rhs;
+    }
+
+    return *this;
+}
+
+
+// explicit template instantiations
+template class Scalar3<double>;
+template class Scalar3<uint64_t>;
--- src/fields/scalar3.hpp
+++ src/fields/scalar3.hpp
@@ -1,12 +1,70 @@
 #ifndef FIELDS_SCALAR3_HPP
 #define FIELDS_SCALAR3_HPP
 
+#include <cstddef>
+#include <cstdint>
 
+#include "triplet.hpp"
 
-class Scalar3 {
+template <typename T>
+class Scalar3
+{
+public:
+    /*!
+     * Scalar3 constructor
+     * @param num_x number of cells in the x-direction
+     * @param num_y number of cells in the y-direction
+     * @param num_z number of cells in the z-direction
+     * @param value initial value to instantiate all field values to
+     */
+    Scalar3(size_t num_x, size_t num_y, size_t num_z, T value);
 
+    /*!
+     * Scalar3 destructor
+     */
+    ~Scalar3();
+
+    /*!
+     * Scalar3 overloaded () operator for read-write access
+     * @param i index into x-direction elements
+     * @param j index into y-direction elements
+     * @param k index into z-direction elements
+     * @return pointer to element at desired index
+     */
+    T* operator()(size_t i, size_t j, size_t k);
+
+    /*!
+     * Scalar3 overloaded () operator for read access
+     * @param i index into x-direction elements
+     * @param j index into y-direction elements
+     * @param k index into z-direction elements
+     * @return pointer to element at desired index
+     */
+    const T& operator()(size_t i, size_t j, size_t k) const;
+
+    /*!
+     * Scalar3 overloaded assignment operator to spatially constant scalar value
+     * @param rhs spatially constant scalar value
+     * @return reference to existing Scalar3
+     */
+    Scalar3& operator=(const T& rhs);
+
+private:
+    /// 1D array in row-major format containing field data
+    T* data;
+
+    /// number of cells in each direction
+    const Triplet<size_t> num_cells;
+
+    /// number of cells in each row
+    const size_t row_cells;
+
+    /// number of cells in each plane
+    const size_t plane_cells;
+
+    /// number of cells in volume
+    const size_t vol_cells;
 };
-
 
 
 #endif //FIELDS_SCALAR3_HPP
