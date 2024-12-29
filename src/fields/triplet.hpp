c15e623a406a4f39d4ab48c3faf4d7cec05a61ef
Sam <sjwyss@outlook.com>
2024-12-28 22:29:27 -0500

    added simple triplet container

---
+++ src/fields/triplet.hpp
@@ -0,0 +1,17 @@
+#ifndef FIELDS_TRIPLET_HPP
+#define FIELDS_TRIPLET_HPP
+
+template <typename T>
+struct Triplet
+{
+    /// x-component of triplet
+    T x;
+
+    /// y-component of triplet
+    T y;
+
+    /// z-component of triplet
+    T z;
+};
+
+#endif //FIELDS_TRIPLET_HPP
