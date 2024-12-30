400bffeabd32daf8bbd6b6041af5aaea9224eecc
sam <sjwyss@outlook.com>
2024-12-29 20:59:09 -0500

    comments

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 * main driver function and build target
  * @param argc argument count
  * @param argv argument vector
- * @return
+ * @return success code
  */
 int main(int argc, char** argv)
 {
